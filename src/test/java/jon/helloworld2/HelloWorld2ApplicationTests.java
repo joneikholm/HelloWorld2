c3042b8596e28d2dafa13239f2766ea7a8235230
joneikholm <2873703+joneikholm@users.noreply.github.com>
2026-09-09 23:26:57 +0200

    with failing test

--- src/test/java/jon/helloworld2/HelloWorld2ApplicationTests.java
+++ src/test/java/jon/helloworld2/HelloWorld2ApplicationTests.java
@@ -2,12 +2,14 @@ package jon.helloworld2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class HelloWorld2ApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(false);
     }
 
 }
